6593000b715f3f90a31fd47684b29e29ec7cc6d4
https://gitkraken.link/uno <chlgusrms96@naver.com>
2023-08-16 14:34:21 +0900

    main으로 바로 push해보고 안되면 그냥 삭제해버리기

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -8,6 +8,8 @@ public class FastCampusProjectBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FastCampusProjectBoardApplication.class, args);
+
+
     }
 
 }
