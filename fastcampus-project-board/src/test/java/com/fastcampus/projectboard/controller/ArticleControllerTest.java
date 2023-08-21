cfe133e23fa8d0894c838c29ee13042820708fd7
https://gitkraken.link/uno <chlgusrms96@naver.com>
2023-08-21 15:44:29 +0900

    #17 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰 테스트까지 한번에 적용

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,7 +46,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("view Get 게시글 상세 화면")
     @Test
     void boardDetailView() throws Exception {
