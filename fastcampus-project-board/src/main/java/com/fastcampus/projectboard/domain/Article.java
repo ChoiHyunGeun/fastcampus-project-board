9e59c3fd37e49de93e1899607b622bfb31458d10
choihyungeun <chlgusrms96@naver.com>
2024-01-22 13:39:53 +0900

    #32 - equals, hashcode 수정
    인증 기능 구현 후 게시글 수정이 안되는 현상이 있었음.
    jpa는 엔티티 객체를 바로 사용하는게 아니라 구현체를 통해 프록시 인스턴스로 사용하기 때문에 필드에 바로 접근하려고 하면 안될 수도 있다고 함.
    그래서 getter 메소드를 사용해서 필드를 참조하도록 변경함

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -67,13 +67,10 @@ public class Article extends AuditingFields{
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Article article = (Article) o;
-        return id != null && Objects.equals(id, article.id);
+        if (!(o instanceof Article that)) return false;
+        return id != null && id.equals(that.getId());
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+    public int hashCode() { return Objects.hash(id); }
 }
