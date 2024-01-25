aab951e1fa4188aff63b5c74be2875977869757a
https://gitkraken.link/uno <chlgusrms96@naver.com>
2024-01-25 11:35:59 +0900

    #47 - 해시태그 검색 기능 고도화
    해시태그 기능을 고도화함
    1. 단일 등록 > 다수 등록
    2. 게시글에서 직접 해시태그를 입력하지 않고 본문에서 파싱하여 해시태그 추가
    3. 기존에 Article테이블에서 관리되던 hashtag 컬럼을 테이블로 별도로 관리하도록 수정
    4. Article, Hashtag 다대다인 관계를 article_hashtag 중간 테이블로 관리하기 용이하도록 구현
    
    위 변경사항에 관련된 서비스 로직 모두 수정

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -1,11 +1,13 @@
 package com.fastcampus.projectboard.service;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.Hashtag;
 import com.fastcampus.projectboard.domain.UserAccount;
 import com.fastcampus.projectboard.domain.constant.SearchType;
 import com.fastcampus.projectboard.dto.ArticleDto;
 import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
+import com.fastcampus.projectboard.repository.HashtagRepository;
 import com.fastcampus.projectboard.repository.UserAccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //articleRepository의 생성자를 만들고 해당 클래스에 주입시켜주는 역할
 @Slf4j
@@ -26,6 +30,8 @@ public class ArticleService {
 
     private final ArticleRepository articleRepository;
     private final UserAccountRepository userAccountRepository;
+    private final HashtagService hashtagService;
+    private final HashtagRepository hashtagRepository;
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
@@ -61,7 +67,11 @@ public class ArticleService {
 
     public void saveArticle(ArticleDto dto) {
         UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
-        articleRepository.save(dto.toEntity(userAccount));
+        Set<Hashtag> hashtags = renewHashtagsFromContent(dto.content());
+
+        Article article = dto.toEntity(userAccount);
+        article.addHashtags(hashtags);
+        articleRepository.save(article);
     }
 
     public void updateArticle(Long articleId, ArticleDto dto) {
@@ -78,7 +88,24 @@ public class ArticleService {
                 if(dto.title() != null) article.setTitle(dto.title());
                 if(dto.content() != null) article.setContent(dto.content());
 
-                //article.setHashtag(dto.hashtag());
+                //기존에 게시글에 등록되었던 해시태그 조회
+                Set<Long> hashtagIds = article.getHashtags().stream()
+                        .map(Hashtag::getId)
+                        .collect(Collectors.toUnmodifiableSet());
+
+                //기존에 등록된 해시태그 모두 삭제
+                article.clearHashtags();
+                //삭제 정보 바로 DB에 반영 > 등록, 수정, 삭제가 일어날 때 hashtag 테이블에 어떻게 반영이 되는지 아직 이해가 안됨 > 중간 테이블에 간접 반영
+                //위에 코드에서 clearHashtags()가 실행되면 article_hashtag 테이블에서 해당 게시글에 관련된 해시태그 정보는 사라지는 듯
+                articleRepository.flush();
+
+                //어떤 게시물에도 등록되지 않은 해시태그는 hashtag 테이블에서 데이터 삭제
+                // > 중간 테이블에서 조회 했을 때, 어떤 게시글에도 해시태그가 등록되어있지 않다면 해시태그 삭제
+                hashtagIds.forEach(hashtagService::deleteHashtagWithoutArticles);
+
+                //게시글 본문에서 파싱된 hashtag 새롭게 등록
+                Set<Hashtag> hashtags = renewHashtagsFromContent(dto.content());
+                article.addHashtags(hashtags);
             }
             /**
              * 이 함수는 클래스 레벨의 @Transactional이 선언되어 있기 때문에
@@ -91,22 +118,56 @@ public class ArticleService {
 
     }
     public void deleteArticle(long articleId, String userId) {
+        Article article = articleRepository.getReferenceById(articleId);
+        Set<Long> hashtagIds = article.getHashtags().stream()
+                .map(Hashtag::getId)
+                .collect(Collectors.toUnmodifiableSet());
+
         articleRepository.deleteByIdAndUserAccount_UserId(articleId, userId);
+        articleRepository.flush();
+
+        hashtagIds.forEach(hashtagService::deleteHashtagWithoutArticles);
     }
 
     public long getArticleCount() {
         return articleRepository.count();
     }
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
-        if (hashtag == null || hashtag.isBlank()) {
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtagName, Pageable pageable) {
+        if (hashtagName == null || hashtagName.isBlank()) {
             return Page.empty(pageable);
         }
 
-        return articleRepository.findByHashtagNames(null, pageable).map(ArticleDto::from);
+        return articleRepository.findByHashtagNames(List.of(hashtagName), pageable)
+                .map(ArticleDto::from);
     }
 
     public List<String> getHashtags() {
-        return articleRepository.findAllDistinctHashtags();
+        return hashtagRepository.findAllHashtagNames(); // TODO: HashtagService 로 이동을 고려해보자.
+    }
+
+    /**
+     * 게시글 본문에서 hashtag 추출
+     * @param content
+     * @return
+     */
+    private Set<Hashtag> renewHashtagsFromContent(String content) {
+        // 게시글 본문에서 파싱된 해시태그
+        Set<String> hashtagNamesInContent = hashtagService.parseHashtagNames(content);
+        // HashtagName으로 DB에서 조회
+        Set<Hashtag> hashtags = hashtagService.findHashtagsByNames(hashtagNamesInContent);
+        // 실제로 DB에 등록되어 있는 Hashtag는 조회가되어 existingHashtagNames에 담김
+        Set<String> existingHashtagNames = hashtags.stream()
+                .map(Hashtag::getHashtagName)
+                .collect(Collectors.toUnmodifiableSet());
+
+        //DB에서 조회되지 않은 해시태그들은 추가로 hashtags에 넣어서 새로 등록될 수 있도록 하기
+        hashtagNamesInContent.forEach(newHashtagName -> {
+            if (!existingHashtagNames.contains(newHashtagName)) {
+                hashtags.add(Hashtag.of(newHashtagName));
+            }
+        });
+
+        return hashtags;
     }
 }
