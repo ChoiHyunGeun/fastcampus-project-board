9f3e0eac29f4459a05ed4984c4119109c606e8c9
https://gitkraken.link/uno <chlgusrms96@naver.com>
2024-01-30 15:51:35 +0900

    #63 - 회원 엔티티 디자인 수정 : 인증이 없는 상태에서 회원 정보를 DB에 등록하기 위한 생성자 메소드 추가
    인증 정보가 없을 땐 직접 생성자, 수정자 정보를 입력하여
    회원 등록을 할 수 있도록 생성자 메소드를 추가함

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -8,7 +8,7 @@ import lombok.ToString;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true) //부모 클래스에 있는 필드들도 toString의 대상에 집어 넣겠다는 의미
 @Table(indexes = {
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createDate"),
@@ -29,16 +29,43 @@ public class UserAccount extends AuditingFields {
 
     protected UserAccount() {}
 
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createUser) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createUser = createUser;
+        this.updateUser = createUser;
     }
 
+    /**
+     * 이미 인증된 상태 > 기존 회원 > DB에 회원 정보가 있는 경우
+     * 해당 of 메소드를 사용
+     * @param userId
+     * @param userPassword
+     * @param email
+     * @param nickname
+     * @param memo
+     * @return
+     */
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return new UserAccount(userId, userPassword, email, nickname, memo, null);
+    }
+
+    /**
+     * 생성자 정보가 없는 경우 > ex) 회원가입하는 경우
+     * 해당 of 메소드를 사용한다.
+     * @param userId
+     * @param userPassword
+     * @param email
+     * @param nickname
+     * @param memo
+     * @param createUser
+     * @return
+     */
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createUser) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createUser);
     }
 
     @Override
